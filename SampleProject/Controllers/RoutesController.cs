5d346f174906c676060dbe6499fa9336c538d240
Adam Stachowicz <saibamenppl@gmail.com>
2023-01-18 21:02:46 +0100

    Fix RoutesController for .NET 6
    And add `DisplayName`

--- SampleProject/Controllers/RoutesController.cs
+++ SampleProject/Controllers/RoutesController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ActionConstraints;
+using Microsoft.AspNetCore.Mvc.Controllers;
 using Microsoft.AspNetCore.Mvc.Infrastructure;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,31 +9,33 @@ using System.Text.Json;
 namespace RouteDebugging.Controllers
 {
     [Route("[controller]/[action]")]
-    public class Routes2Controller : Controller
+    public class RoutesController : Controller
     {
         private readonly IActionDescriptorCollectionProvider _actionDescriptorCollectionProvider;
 
         public Routes2Controller(IActionDescriptorCollectionProvider actionDescriptorCollectionProvider)
         {
-            this._actionDescriptorCollectionProvider = actionDescriptorCollectionProvider;
+            _actionDescriptorCollectionProvider = actionDescriptorCollectionProvider;
         }
 
         [HttpGet]
         [HttpPut]
         public IActionResult Index()
         {
-            var routes = _actionDescriptorCollectionProvider.ActionDescriptors.Items.Select(x => new {
-                    Controller = x.RouteValues["Controller"],
-                    Action = x.RouteValues["Action"],
-                    Parameters = x.Parameters.Select(param => new
-                    {
-                        Name = param.Name,
-                        Type = param.ParameterType.Name,
-                    }),
-                    Template = x.AttributeRouteInfo?.Template,
-                    Name = x.AttributeRouteInfo?.Name,
-                    HttpMethods = string.Join(", ", x.ActionConstraints?.OfType<HttpMethodActionConstraint>().First().HttpMethods ?? new List<string>()),
-                }).OrderBy(x => x.Controller).ToList();
+            var routes = _actionDescriptorCollectionProvider.ActionDescriptors.Items.OfType<ControllerActionDescriptor>().Select(x => new
+            {
+                Controller = x.ControllerName,
+                Action = x.ActionName,
+                DisplayName = x.DisplayName,
+                Parameters = x.Parameters.Select(param => new
+                {
+                    Name = param.Name,
+                    Type = param.ParameterType.Name,
+                }),
+                Template = x.AttributeRouteInfo?.Template,
+                Name = x.AttributeRouteInfo?.Name,
+                HttpMethods = string.Join(", ", x.ActionConstraints?.OfType<HttpMethodActionConstraint>().FirstOrDefault()?.HttpMethods ?? new List<string>()),
+            }).OrderBy(x => x.Controller).ToList();
 
             return Json(routes, new JsonSerializerOptions
             {
