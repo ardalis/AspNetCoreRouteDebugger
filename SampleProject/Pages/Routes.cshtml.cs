81f63a391251dcb3966b0e595026c435041c7924
Steve Smith <steve@kentsmiths.com>
2024-11-24 15:57:11 -0500

    Adding NRT support

--- SampleProject/Pages/Routes.cshtml.cs
+++ SampleProject/Pages/Routes.cshtml.cs
@@ -7,50 +7,53 @@ namespace RouteDebugging.Pages;
 
 public class RoutesModel : PageModel
 {
-    private readonly IActionDescriptorCollectionProvider _actionDescriptorCollectionProvider;
+  private readonly IActionDescriptorCollectionProvider _actionDescriptorCollectionProvider;
 
-    public RoutesModel(IActionDescriptorCollectionProvider actionDescriptorCollectionProvider)
-    {
-        this._actionDescriptorCollectionProvider = actionDescriptorCollectionProvider;
-    }
+  public RoutesModel(IActionDescriptorCollectionProvider actionDescriptorCollectionProvider)
+  {
+    this._actionDescriptorCollectionProvider = actionDescriptorCollectionProvider;
+  }
 
-    public List<RouteInfo> Routes { get; set; } = new();
+  public List<RouteInfo> Routes { get; set; } = new();
 
-    public void OnGet()
-    {
-        var PageRoutes = _actionDescriptorCollectionProvider.ActionDescriptors.Items.OfType<PageActionDescriptor>()
-                .Select(x => new RouteInfo
-                {
-                    Action = "",
-                    Controller = x.DisplayName,
-                    Name = x.AttributeRouteInfo?.Name,
-                    Template = x.AttributeRouteInfo?.Template,
-                    Constraint = x.ActionConstraints == null ? "" : JsonSerializer.Serialize(x.ActionConstraints),
-                    RouteValues = string.Join(',', x.RouteValues)
-                })
-            .OrderBy(r => r.Template);
+  public void OnGet()
+  {
+    // DisplayName
+    var PageRoutes = _actionDescriptorCollectionProvider.ActionDescriptors.Items.OfType<PageActionDescriptor>()
+            .Select(x => new RouteInfo
+            {
+              Action = "",
+              Controller = x.DisplayName ?? string.Empty,
+              Name = x.AttributeRouteInfo?.Name ?? string.Empty,
+              Template = x.AttributeRouteInfo?.Template ?? string.Empty,
+              Constraint = x.ActionConstraints == null ? "" : JsonSerializer.Serialize(x.ActionConstraints),
+              RouteValues = string.Join(',', x.RouteValues)
+            })
+        .OrderBy(r => r.Template)
+        .ToList();
 
-        var ViewRoutes = _actionDescriptorCollectionProvider.ActionDescriptors.Items.OfType<ControllerActionDescriptor>()
-                .Select(x => new RouteInfo
-                {
-                    Action = x.RouteValues["Action"],
-                    Controller = x.RouteValues["Controller"],
-                    Name = x.AttributeRouteInfo?.Name,
-                    Template = x.AttributeRouteInfo?.Template,
-                    Constraint = x.ActionConstraints == null ? "" : JsonSerializer.Serialize(x.ActionConstraints),
-                })
-            .OrderBy(r => r.Template);
+    var ViewRoutes = _actionDescriptorCollectionProvider.ActionDescriptors.Items.OfType<ControllerActionDescriptor>()
+            .Select(x => new RouteInfo
+            {
+              Action = x.RouteValues["Action"] ?? string.Empty,
+              Controller = x.RouteValues["Controller"] ?? string.Empty,
+              Name = x.AttributeRouteInfo?.Name ?? string.Empty,
+              Template = x.AttributeRouteInfo?.Template ?? string.Empty,
+              Constraint = x.ActionConstraints == null ? "" : JsonSerializer.Serialize(x.ActionConstraints),
+            })
+        .OrderBy(r => r.Template)
+        .ToList();
 
-        Routes = PageRoutes.Concat(ViewRoutes).ToList();
-    }
+    Routes = PageRoutes.Concat(ViewRoutes).ToList();
+  }
 
-    public class RouteInfo
-    {
-        public string Template { get; set; }
-        public string Name { get; set; }
-        public string Controller { get; set; }
-        public string Action { get; set; }
-        public string Constraint { get; set; }
-        public string RouteValues {  get; set; }
-    }
+  public class RouteInfo
+  {
+    public string Template { get; set; } = string.Empty;
+    public string Name { get; set; } = string.Empty;
+    public string Controller { get; set; } = string.Empty;
+    public string Action { get; set; } = string.Empty;
+    public string Constraint { get; set; } = string.Empty;
+    public string RouteValues { get; set; } = string.Empty;
+  }
 }
